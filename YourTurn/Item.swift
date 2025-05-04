4875f8929e5734c5863820528daaee717c7b096d
Ruben <55102143+boulomiel@users.noreply.github.com>
2025-05-04 14:59:52 +0300

    init

---
+++ YourTurn/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  YourTurn
+//
+//  Created by Ruben Mimoun on 04/05/2025.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ YourTurnTests/YourTurnTests.swift
@@ -0,0 +1,17 @@
+//
+//  YourTurnTests.swift
+//  YourTurnTests
+//
+//  Created by Ruben Mimoun on 04/05/2025.
+//
+
+import Testing
+@testable import YourTurn
+
+struct YourTurnTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
