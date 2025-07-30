f797b403ecb1f5a308d06f091dfe271634b26a19
boulomiel <55102143+boulomiel@users.noreply.github.com>
2025-07-30 09:04:10 +0300

    Corrupted Build fix
    Fix corrupted build base on a missing tests target

--- SBExtensions/Package.swift
+++ SBExtensions/Package.swift
@@ -10,7 +10,7 @@ let package = Package(
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "SBExtensions",
-            targets: ["SBExtensions", "SBExtensionsTests"]
+            targets: ["SBExtensions"]
         ),
     ],
     targets: [
